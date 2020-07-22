cf959e9652ff84540204ad0871e97cbe599a345b
vagrant <kf0830s@gmail.com>
2020-07-22 10:25:01 +0000

    Add: reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,6 @@ Rails.application.routes.draw do
     resource :likes, only: [:create, :destroy]
   end
   resources :gelandes, only: [:index, :show]
+  resources :reviews, only: [:create, :show, :destroy]
 
 end

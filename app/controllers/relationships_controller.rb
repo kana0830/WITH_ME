909735d78c881139d5657a47da13b27aaa5cbba4
vagrant <kf0830s@gmail.com>
2020-07-23 11:34:13 +0000

    Add: follow function

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,13 @@ class RelationshipsController < ApplicationController
     # フォローする
     def follow
       current_user.follow(params[:id])
-      redirect_to users_path
+      redirect_back(fallback_location: root_path)
     end
   
     # アンフォローする
     def unfollow
       current_user.unfollow(params[:id])
-      redirect_to users_path
+      redirect_back(fallback_location: root_path)
     end
 end
 
